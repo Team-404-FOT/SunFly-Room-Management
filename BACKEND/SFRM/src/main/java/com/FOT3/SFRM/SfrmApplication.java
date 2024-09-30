340eb8e3f6f1ce51380d644fc931b1bf73d586dc
Thisara Bandara <thisarabandara2@gmail.com>
2024-09-30 23:06:57 +0530

    create springboot project

---
+++ BACKEND/SFRM/src/main/java/com/FOT3/SFRM/SfrmApplication.java
@@ -0,0 +1,13 @@
+package com.FOT3.SFRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfrmApplication.class, args);
+	}
+
+}
---
+++ BACKEND/SFRM/src/test/java/com/FOT3/SFRM/SfrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FOT3.SFRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
