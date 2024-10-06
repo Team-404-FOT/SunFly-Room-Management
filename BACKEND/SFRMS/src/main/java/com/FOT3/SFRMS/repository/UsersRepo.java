2d29c56b6299d0f66c2649bd2f22f9ec1ec11f05
Thisara Bandara <thisarabandara2@gmail.com>
2024-10-06 12:45:08 +0530

    User Management Backend

---
+++ BACKEND/SFRMS/src/main/java/com/FOT3/SFRMS/repository/UsersRepo.java
@@ -0,0 +1,14 @@
+package com.FOT3.SFRMS.repository;
+
+import com.FOT3.SFRMS.entity.OurUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsersRepo extends JpaRepository<OurUsers, Integer> {
+
+    Optional<OurUsers> findByEmail(String email);
+
+}
