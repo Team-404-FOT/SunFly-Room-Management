ac7f2c82b527a15e117146227479394ba49c815b
Thisara Bandara <thisarabandara2@gmail.com>
2024-10-10 17:39:15 +0530

    file creation

---
+++ FRONTEND/SFRMS-Front/src/components/service/UsersService.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+class UsersService{
+    static BASE_URL = "http://localhost:8080"
+
+    static async login(email,password){
+            try {
+                
+            } catch (error) {
+                throw error;
+            }
+
+    }
+}
