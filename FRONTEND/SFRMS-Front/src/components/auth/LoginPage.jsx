bdd0ec86304c6dce0dc4976085f1123b7d107d18
Thisara Bandara <thisarabandara2@gmail.com>
2024-10-11 22:44:20 +0530

    update tailwind / flowbite config

--- FRONTEND/SFRMS-Front/src/components/auth/LoginPage.jsx
+++ FRONTEND/SFRMS-Front/src/components/auth/LoginPage.jsx
@@ -1,2 +1,13 @@
 import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import UsersService from "../../services/UsersService";
 
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
