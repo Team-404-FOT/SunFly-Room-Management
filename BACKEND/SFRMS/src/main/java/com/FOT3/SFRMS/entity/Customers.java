a4ce875b29afc1c72f2e467ff59a568cedb234cf
Thisara Bandara <thisarabandara2@gmail.com>
2024-10-25 09:03:23 +0530

    create models(entity)

--- BACKEND/SFRMS/src/main/java/com/FOT3/SFRMS/entity/Customers.java
+++ BACKEND/SFRMS/src/main/java/com/FOT3/SFRMS/entity/Customers.java
@@ -1,4 +1,30 @@
 package com.FOT3.SFRMS.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customers")
+@Data
+@Getter
+@Setter
 public class Customers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cusId;
+    private String firstName;
+    private String lastName;
+    private String nic;
+    private String phoneNumber;
+
+    @ManyToOne // Many customers can be associated with one user
+    @JoinColumn(name = "userId", nullable = false)
+    private OurUsers user; // This will hold the user who added the customer
+
+
+
+
 }
